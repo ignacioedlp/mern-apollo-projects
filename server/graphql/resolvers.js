180f200c9e04f81ac948b885da2daea4d7429f68
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2023-02-12 12:55:04 -0300

    create project

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,5 +1,17 @@
+import Project from "../models/Project.js";
+
 export const resolvers = {
   Query: {
     hello: () => "Hello world!",
+  },
+  Mutation: {
+    createProject: async (_, { name, description }) => {
+      const project = new Project({
+        name,
+        description,
+      });
+      await project.save();
+      return project;
+    },
   },
 };
---
+++ server/models/Project.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const ProjectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Project", ProjectSchema);
