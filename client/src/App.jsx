eb79865820de779a88964adcd0e5025fcd0035f8
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2023-02-12 14:47:48 -0300

    setup frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Projects from "./pages/Projects";
+import ProjectDetails from "./pages/ProjectDetails";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/projects" />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:id" element={<ProjectDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
